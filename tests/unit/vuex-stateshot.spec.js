ccce4d6f1186f395e93c2eb2203f1122303673ef
xiaoluoboding <xiaoluoboding@gmail.com>
2019-12-23 13:00:12 +0800

    test: write the namespaced module under namespaced module test case

--- tests/unit/vuex-stateshot.spec.js
+++ tests/unit/vuex-stateshot.spec.js
@@ -4,7 +4,8 @@ import { logshot, createApp } from './helper'
 
 describe.each([
   ['Test root module', 'rootModule', 'first'],
-  ['Test namespaced module', 'global', 'second']
+  ['Test namespaced module', 'global', 'second'],
+  ['Test namespaced module under namespaced module', 'global/widget', 'third']
 ])('VuexStateshot', (description, namespace, flag) => {
   if (flag === 'first') {
     describe(description, () => {
@@ -140,6 +141,98 @@ describe.each([
         const { global } = await vm.redo()
         expect(global.color).toBe('#fff')
         expect(global.lang).toBe('zh')
+        expect(vm.hasUndo).toBeTruthy()
+        expect(vm.hasRedo).toBeTruthy()
+        expect(vm.undoCount).toBe(1)
+        expect(vm.redoCount).toBe(2)
+      })
+
+      it('call reset history', () => {
+        vm.reset()
+        expect(vm.hasUndo).toBeFalsy()
+        expect(vm.hasRedo).toBeFalsy()
+        expect(vm.undoCount).toBe(0)
+        expect(vm.redoCount).toBe(0)
+        expect(vm.historyLength).toBe(0)
+      })
+    })
+  }
+
+  if (flag === 'third') {
+    describe(description, () => {
+      let vm = null
+
+      beforeAll(() => {
+        const wrapper = createApp(namespace)
+        vm = wrapper.vm
+      })
+
+      it('sets initial states', () => {
+        expect(vm.hasUndo).toBeFalsy()
+        expect(vm.hasRedo).toBeFalsy()
+        expect(vm.undoCount).toBe(0)
+        expect(vm.redoCount).toBe(0)
+        expect(vm.historyLength).toBe(1)
+      })
+
+      it('dispatch a action on global/widget module', async () => {
+        expect(vm.lang).toBe('fr')
+        vm.setLang('zh')
+        expect(vm.lang).toBe('zh')
+        await flushPromises()
+        // logshot(vm)
+        expect(vm.hasUndo).toBeTruthy()
+        expect(vm.hasRedo).toBeFalsy()
+        expect(vm.undoCount).toBe(1)
+        expect(vm.redoCount).toBe(0)
+        expect(vm.historyLength).toBe(2)
+      })
+
+      it('commit a mutation on global/widget module', async () => {
+        expect(vm.color).toBe('#000')
+        vm.changeColor('#4af')
+        expect(vm.color).toBe('#4af')
+        await flushPromises()
+        expect(vm.hasUndo).toBeTruthy()
+        expect(vm.hasRedo).toBeFalsy()
+        expect(vm.undoCount).toBe(2)
+        expect(vm.redoCount).toBe(0)
+        expect(vm.historyLength).toBe(3)
+      })
+
+      it('call custom action(unsubscribed)', async () => {
+        expect(vm.color).toBe('#4af')
+        // begin custom action
+        vm.setState({
+          color: '#af4'
+        })
+        vm.$stateshot.syncState()
+        // end custom action
+        expect(vm.color).toBe('#af4')
+        await flushPromises()
+        expect(vm.hasUndo).toBeTruthy()
+        expect(vm.hasRedo).toBeFalsy()
+        expect(vm.undoCount).toBe(3)
+        expect(vm.redoCount).toBe(0)
+        expect(vm.historyLength).toBe(4)
+      })
+
+      it('call undo multiple times', async () => {
+        await vm.undo()
+        await vm.undo()
+        const { global: { widget } } = await vm.undo()
+        expect(widget.color).toBe('#000')
+        expect(widget.lang).toBe('fr')
+        expect(vm.hasUndo).toBeFalsy()
+        expect(vm.hasRedo).toBeTruthy()
+        expect(vm.undoCount).toBe(0)
+        expect(vm.redoCount).toBe(3)
+      })
+
+      it('call redo', async () => {
+        const { global: { widget } } = await vm.redo()
+        expect(widget.color).toBe('#000')
+        expect(widget.lang).toBe('zh')
         expect(vm.hasUndo).toBeTruthy()
         expect(vm.hasRedo).toBeTruthy()
         expect(vm.undoCount).toBe(1)
